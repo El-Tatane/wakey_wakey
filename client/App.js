90d34db78ac927e55f97d0db111c3b54c90ee9e5
pythalex <hyorvenn@protonmail.com>
2020-04-10 15:31:06 +0200

    Contenu de la page change en fonction du choix de menu

--- client/App.js
+++ client/App.js
@@ -1,20 +1,64 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
-import Flux from "./components/Flux"
-import Menu from "./components/Menu"
+import {Text, View, StyleSheet, Picker} from 'react-native'
 
-export default function App() {
+export default class App extends React.Component {
 
-    return (
-        <View style={styles.space}>
-            <Menu/>
-            <Flux/>
-        </View>
+    constructor(props) {
+        super(props);
+        this.state = {
+            flux: "Météo"
+        }
+    }
 
-    );
+
+    setFlux(flux){
+        this.setState({flux});
+    }
+
+    fluxSource(){
+        switch(this.state.flux){
+            case "Météo":
+                return <Text>Meteo</Text>
+            case "Actualité":
+                return <Text>Actualite</Text>
+            default:
+                throw "Etat de flux non reconnu";
+        }
+    }
+
+    render() {
+        return(
+            <View style={{marginTop: 32}}>
+                <View style={styles.debug}>
+                    <Picker
+                        selectedValue={this.state.flux}
+                        onValueChange={(itemValue, itemPosition) => {
+                            this.setFlux(itemValue);
+                            console.log("Change flux to " + itemValue);
+                        }}
+                        mode="dropdown"
+                    >
+                        <Picker.Item label="Météo" value="Météo"/>
+                        <Picker.Item label="Actualité" value="Actualité"/>
+                    </Picker>
+                </View>
+                <View>
+                    {this.fluxSource()}
+                </View>
+            </View>
+        )
+    }
+
+
 }
 
 const styles = StyleSheet.create({
+
+    debug: {
+        borderWidth: 2,
+        borderColor: "red"
+    },
+
     space : {
         marginTop: 32 // 1 em d'espace pour la bar d'outils du téléphone
     }
